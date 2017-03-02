37f677c44393ec11df981fa55cd5731b21428162
Dotan Dimet <dotan@corky.net>
2017-03-02 16:29:37 +0200

    add dummy Vuex store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 
 new Vue({
   router,
+  store,
   App,
   render(h){return h(App)}
 }).$mount('#app')
